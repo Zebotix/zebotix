da590fd268f40eedb11c43695ff798b0f805229c
ZeeshanZk09 <mzeeshankhan0988@gmail.com>
2025-01-31 06:01:40 +0500

    zebotix-webite

---
+++ src/components/Header.tsx
@@ -0,0 +1,172 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import "./Header.css";
+
+// xs sm md lg xl 2xl 3xl 4xl 5xl 6xl 7xl 8xl 9xl
+const Header = () => {
+  interface navLinks {
+    label: string;
+    path: string;
+    id: number;
+  }
+
+  const navLinks: navLinks[] = [
+    {
+      label: "Home",
+      path: "/",
+      id: 1,
+    },
+    {
+      label: "About",
+      path: "/",
+      id: 2,
+    },
+    {
+      label: "Product",
+      path: "/product",
+      id: 3,
+    },
+  ];
+
+  const [search, setSearch] = useState("");
+
+  return (
+    <header className="flex flex-col bg-white text-black  h-24 w-screen justify-between items-center p-2 shadow-lg shadow-black">
+      <nav className="lg:hidden w-screen px-2 overflow-hidden flex justify-between">
+        <div className="flex flex-row-reverse w-full items-center justify-between px-2">
+          <Link href={`/`}>
+            <Image
+              className="  w-7 h-7 md:w-14 md:h-14 "
+              src="/Zebotix.png"
+              alt="logo"
+              width={500}
+              height={500}
+            />
+          </Link>
+
+          <div className="flex flex-row-reverse gap-4">
+            <button type="button" title="search"></button>
+            <Link href={"/"}>
+              <button
+                type="button"
+                className="border  px-2 py-1 rounded-md outline-none"
+              >
+                login
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="hidden">
+          <input
+            className="border-none rounded-full px-2"
+            title="Search here"
+            type="search"
+            name="search"
+            id="navInputSearch"
+          />
+        </div>
+      </nav>
+      <nav className="lg:hidden ">
+        <div className="w-screen px-4 flex justify-between ">
+          <ul className="flex justify-between w-2/3">
+            <li className="border border-gray-800 rounded-md p-1">
+              <Link href={`/`} className="flex text-xs items-center gap-1">
+                Languages{" "}
+                <span>
+                  <Image
+                    className="w-4 h-4"
+                    src={"/icons8-globe.gif"}
+                    alt="down-arrow"
+                    width={100}
+                    height={100}
+                  />
+                </span>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/`}>Jobs</Link>
+            </li>
+            <li>
+              <Link href={`/`}>Services</Link>
+            </li>
+          </ul>
+          <div className="w-1/3 flex gap-2 items-center justify-end">
+            <svg
+              className=" "
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 11 21 L 11 15 L 13 15 L 13 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z M 12 4.7910156 L 18 10.191406 L 18 11 L 18 19 L 15 19 L 15 13 L 9 13 L 9 19 L 6 19 L 6 10.191406 L 12 4.7910156 z"></path>
+            </svg>
+            <svg
+              className="w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="50"
+              height="50"
+              viewBox="0 0 50 50"
+            >
+              <path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path>
+            </svg>
+          </div>
+        </div>
+      </nav>
+
+      {/* ----------------- desktop view ----------------- */}
+
+      <nav className="hidden md:flex md:justify-between">
+        <div className="hidden lg:flex bg-gray-200 px-2 md:px-4 py-1 md:py-2 rounded-full overflow-hidden w-1/3  md:w-1/2 outline-none lg:gap-2">
+          <input
+            type="search"
+            className="bg-gray-200 px-2 md:px-4 text-sm rounded-full font-thin md:text-xl outline-none w-full"
+            name="search"
+            id="search"
+            placeholder="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button type="button" title="search">
+            <Image
+              className="w-5 h-5 md:w-6 md:h-6"
+              src={`/search.svg`}
+              alt={`search`}
+              height={100}
+              width={100}
+            />
+          </button>
+        </div>
+        <div></div>
+        <div className=" flex gap-4 ">
+          {navLinks.map(({ label, path, id }) => (
+            <Link
+              key={id}
+              href={path}
+              className="link-with-border font-medium text-sm md:text-lg"
+            >
+              {label}
+            </Link>
+          ))}
+        </div>
+        <div className="login">
+          <Link href={"/"}>
+            <button
+              type="button"
+              className="border-b-2 border-gray-500 px-2 py-1 rounded-md outline-none"
+            >
+              login
+            </button>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
