df28226c20fa7adf18c9b23b2c8d45e4d2b1222c
ZeeshanZk09 <mzeeshankhan0988@gmail.com>
2026-07-07 00:33:55 +0500

    feat: add prisma seed script for admin user initialization and update package scripts

--- prisma/seeds/users.ts
+++ prisma/seeds/users.ts
@@ -1,22 +1,31 @@
 /* eslint-disable no-console */
+import "dotenv/config";
+import { hash } from "bcryptjs";
+
 import { type PrismaClient } from "../../src/generated/prisma/client";
 
 export async function seedUsers(prisma: PrismaClient) {
   // Check if Admin exists
   const existingAdmin = await prisma.user.findUnique({
-    where: { email: "mzeeshankhan0988@gmail.com" },
+    where: { email: process.env.ADMIN_EMAIL },
   });
 
   if (existingAdmin) {
     return existingAdmin;
   }
 
+  if (!process.env.ADMIN_PASSWORD) {
+    return null;
+  }
+
+  const passwordHash = await hash(process.env.ADMIN_PASSWORD, 10);
+
   // Create Admin User
   const admin = await prisma.user.create({
     data: {
       name: "Admin",
-      email: "mzeeshankhan0988@gmail.com",
-      passwordHash: "$2a$12$R9h/cIPz0gi.URNNX3kh2OPST9/PgBkqquzi.Ss7KIUgO2t0jWMUW", // default password123
+      email: process.env.ADMIN_EMAIL ?? "",
+      passwordHash,
       role: "admin",
     },
   });
